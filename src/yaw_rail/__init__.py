a79fccaaf8f41e47772b650d9da54fe4141e58fc
Jan Luca van den Busch <jl.vdb@astro.rub.de>
2023-07-27 10:01:36 -0700

    added missing imports to __init__.py

--- src/yaw_rail/__init__.py
+++ src/yaw_rail/__init__.py
@@ -1,1 +1,11 @@
+from yaw_rail.config import SetupConfig
+from yaw_rail.correlators import YAWAutoCorr, YAWCrossCorr
+from yaw_rail.estimators import YAWEstimator
+
+__all__ = [
+    "SetupConfig",
+    "YAWAutoCorr",
+    "YAWCrossCorr",
+    "YAWEstimator",
+]
 __version__ = "0.0.1"
