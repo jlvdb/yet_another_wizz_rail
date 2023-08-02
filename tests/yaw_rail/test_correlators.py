489ad14c83daa68eefc8a6a88ce859cb6b700d93
Jan Luca van den Busch <jl.vdb@astro.rub.de>
2023-08-02 12:22:14 -0700

    added test for building a catalog

--- tests/yaw_rail/test_correlators.py
+++ tests/yaw_rail/test_correlators.py
@@ -1,5 +1,8 @@
+import numpy.testing as npt
+import yaw
 from numpy.random import default_rng
 from pytest import fixture
+from rail.core.data import PqHandle
 from rail.core.stage import RailStage
 from yaw import UniformRandoms
 
@@ -54,17 +57,44 @@ class TestYAWCorrelatorBase:
         )
         DS.clear()
 
-
-"""
-from rail.core.data import PqHandle
+    def test_build_catalog(self, data, tmp_path):
+        DS = RailStage.data_store
 
         # create input files with random points
-        path = str(tmp_path / f"data.pqt")
+        path = str(tmp_path / "data.pq")
+        data["patch"] = 0
         data.to_parquet(path)
-        path = str(tmp_path / f"rand.pqt")
-        rand.to_parquet(path)
+        data.to_parquet(path + "t")
 
         # add files to datastore
-        data_handle = DS.read_file("data", PqHandle, "data.pqt")
-        rand_handle = DS.read_file("rand", PqHandle, "rand.pqt")
-"""
+        data_handle = DS.read_file("data", PqHandle, path)
+
+        stage = YAWAutoCorr.make_stage(
+            rmin=100.0,
+            rmax=1000.0,
+            zmin=0.01,
+            zmax=3.0,
+            zbin_num=30,
+            ra_name="ra",
+            dec_name="dec",
+            patch_name="patch",
+            redshift_name="z",
+            cache_path=str(tmp_path),
+        )
+
+        assert stage.build_catalog("any_value", None) is None
+
+        ref_cat = yaw.NewCatalog().from_file(
+            path + "t",
+            ra="ra",
+            dec="dec",
+            redshift="z",
+            patches="patch",
+        )
+
+        stage_cat = stage.build_catalog("data", data_handle)
+
+        for attr in ("ra", "dec", "redshifts", "patch"):
+            npt.assert_array_equal(getattr(ref_cat, attr), getattr(stage_cat, attr))
+
+        DS.clear()
